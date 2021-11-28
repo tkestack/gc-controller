40f77a4af9a390ebe2df84b4452396c84f860cb2
lutzowguo <lutzowguo@tencent.com>
2021-11-28 23:33:11 +0800

    project init

---
+++ app/options/options.go
@@ -0,0 +1,147 @@
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package options provides the flags used for the controller manager.
+//
+package options
+
+import (
+	gcconfig "github.com/tkestack/gc-controller/app/config"
+	v1 "k8s.io/api/core/v1"
+	clientset "k8s.io/client-go/kubernetes"
+	clientgokubescheme "k8s.io/client-go/kubernetes/scheme"
+	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/record"
+	cliflag "k8s.io/component-base/cli/flag"
+	componentbaseconfig "k8s.io/component-base/config"
+	"k8s.io/component-base/config/options"
+	"k8s.io/kubernetes/pkg/controller/garbagecollector"
+	garbagecollectorconfig "k8s.io/kubernetes/pkg/controller/garbagecollector/config"
+
+	// add the kubernetes feature gates
+	_ "k8s.io/kubernetes/pkg/features"
+)
+
+const (
+	// KubeControllerManagerUserAgent is the userAgent name when starting kube-controller managers.
+	KubeControllerManagerUserAgent = "kube-controller-manager"
+)
+
+type GarbageCollectorControllerOptions struct {
+	LeaderElection         componentbaseconfig.LeaderElectionConfiguration
+	ClientConnection       componentbaseconfig.ClientConnectionConfiguration
+	EnableGarbageCollector bool
+	ConcurrentGCSyncs      int32
+	GCIgnoredResources     []garbagecollectorconfig.GroupResource
+	GCGroup                string
+	Master                 string
+	Kubeconfig             string
+}
+
+// NewKubeControllerManagerOptions creates a new KubeControllerManagerOptions with a default config.
+func NewPlatformGcControllerOptions() (*GarbageCollectorControllerOptions, error) {
+	g := GarbageCollectorControllerOptions{
+		ClientConnection:       componentbaseconfig.ClientConnectionConfiguration{},
+		EnableGarbageCollector: true,
+		ConcurrentGCSyncs:      5,
+		GCIgnoredResources:     []garbagecollectorconfig.GroupResource{},
+		Master:                 "",
+		Kubeconfig:             "",
+	}
+	gcIgnoredResources := make([]garbagecollectorconfig.GroupResource, 0, len(garbagecollector.DefaultIgnoredResources()))
+	for r := range garbagecollector.DefaultIgnoredResources() {
+		gcIgnoredResources = append(gcIgnoredResources, garbagecollectorconfig.GroupResource{Group: r.Group, Resource: r.Resource})
+	}
+	g.GCIgnoredResources = gcIgnoredResources
+	g.LeaderElection.ResourceName = "platfor-gc-controller"
+	g.LeaderElection.ResourceNamespace = "kube-system"
+
+	return &g, nil
+}
+
+// Flags returns flags for a specific APIServer by section name
+func (g *GarbageCollectorControllerOptions) Flags() cliflag.NamedFlagSets {
+	fss := cliflag.NamedFlagSets{}
+	genericfs := fss.FlagSet("generic")
+	genericfs.StringVar(&g.ClientConnection.ContentType, "kube-api-content-type", g.ClientConnection.ContentType, "Content type of requests sent to apiserver.")
+	genericfs.Float32Var(&g.ClientConnection.QPS, "kube-api-qps", g.ClientConnection.QPS, "QPS to use while talking with kubernetes apiserver.")
+	genericfs.Int32Var(&g.ClientConnection.Burst, "kube-api-burst", g.ClientConnection.Burst, "Burst to use while talking with kubernetes apiserver.")
+	genericfs.StringVar(&g.Master, "master", g.Master, "The address of the Kubernetes API server (overrides any value in kubeconfig).")
+	genericfs.StringVar(&g.Kubeconfig, "kubeconfig", g.Kubeconfig, "Path to kubeconfig file with authorization and master location information.")
+	options.BindLeaderElectionFlags(&g.LeaderElection, genericfs)
+
+	gcfs := fss.FlagSet("garbagecollector controller")
+	gcfs.Int32Var(&g.ConcurrentGCSyncs, "concurrent-gc-syncs", g.ConcurrentGCSyncs, "Number of garbage collector workers that are allowed to sync concurrently.")
+	gcfs.BoolVar(&g.EnableGarbageCollector, "enable-garbage-collector", g.EnableGarbageCollector, "Enables the generic garbage collector. MUST be synced with the corresponding flag of the kube-apiserver.")
+	gcfs.StringVar(&g.GCGroup, "gcgroup", "platform.tkestack.io", "specify resoure group to be gc,useage:--gcgroup=platform.tkestack.io")
+	return fss
+}
+
+// ApplyTo fills up controller manager config with options.
+func (g *GarbageCollectorControllerOptions) ApplyTo(cfg *gcconfig.Config) error {
+	if g == nil {
+		return nil
+	}
+	cfg.ClientConnection = g.ClientConnection
+	cfg.LeaderElection = g.LeaderElection
+
+	cfg.ConcurrentGCSyncs = g.ConcurrentGCSyncs
+	cfg.GCIgnoredResources = g.GCIgnoredResources
+	cfg.EnableGarbageCollector = g.EnableGarbageCollector
+	cfg.GCGroup = g.GCGroup
+	cfg.KubeConfPath = g.Kubeconfig
+
+	return nil
+}
+
+// Config return a controller manager config objective
+func (s GarbageCollectorControllerOptions) Config() (*gcconfig.Config, error) {
+	kubeconfig, err := clientcmd.BuildConfigFromFlags(s.Master, s.Kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	kubeconfig.DisableCompression = true
+	kubeconfig.ContentConfig.AcceptContentTypes = s.ClientConnection.AcceptContentTypes
+	kubeconfig.ContentConfig.ContentType = s.ClientConnection.ContentType
+	kubeconfig.QPS = s.ClientConnection.QPS
+	kubeconfig.Burst = int(s.ClientConnection.Burst)
+
+	client, err := clientset.NewForConfig(restclient.AddUserAgent(kubeconfig, KubeControllerManagerUserAgent))
+	if err != nil {
+		return nil, err
+	}
+
+	eventRecorder := createRecorder(client, KubeControllerManagerUserAgent)
+	c := &gcconfig.Config{
+		Client:        client,
+		Kubeconfig:    kubeconfig,
+		EventRecorder: eventRecorder,
+	}
+	if err := s.ApplyTo(c); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func createRecorder(kubeClient clientset.Interface, userAgent string) record.EventRecorder {
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartStructuredLogging(0)
+	eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: kubeClient.CoreV1().Events("")})
+	return eventBroadcaster.NewRecorder(clientgokubescheme.Scheme, v1.EventSource{Component: userAgent})
+}
---
+++ go.mod
@@ -0,0 +1,55 @@
+module github.com/tkestack/gc-controller
+
+go 1.16
+
+replace (
+	github.com/NetEase-Object-Storage/nos-golang-sdk => github.com/karuppiah7890/nos-golang-sdk v0.0.0-20191116042345-0792ba35abcc
+	go.etcd.io/etcd => go.etcd.io/etcd v0.5.0-alpha.5.0.20200401174654-e694b7bb0875
+	//google.golang.org/grpc => google.golang.org/grpc v1.26.0
+	k8s.io/api => k8s.io/api v0.22.3
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.22.3
+	k8s.io/apimachinery => k8s.io/apimachinery v0.22.3
+	k8s.io/apiserver => k8s.io/apiserver v0.22.3
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.22.3
+	k8s.io/client-go => k8s.io/client-go v0.22.3
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.22.3
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.22.3
+	k8s.io/code-generator => k8s.io/code-generator v0.22.3
+	k8s.io/component-base => k8s.io/component-base v0.22.3
+	k8s.io/component-helpers => k8s.io/component-helpers v0.22.3
+	k8s.io/controller-manager => k8s.io/controller-manager v0.22.3
+	k8s.io/cri-api => k8s.io/cri-api v0.22.3
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.22.3
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.22.3
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.22.3
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.22.3
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.22.3
+	k8s.io/kubectl => k8s.io/kubectl v0.22.3
+	k8s.io/kubelet => k8s.io/kubelet v0.22.3
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.22.3
+	k8s.io/metrics => k8s.io/metrics v0.22.3
+	k8s.io/mount-utils => k8s.io/mount-utils v0.22.3
+	k8s.io/pod-security-admission => k8s.io/pod-security-admission v0.22.3
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.22.3
+	tkestack.io/tke => tkestack.io/tke v1.6.1-0.20211126034328-76efe488fd3a
+)
+
+require (
+	github.com/go-bindata/go-bindata v3.1.2+incompatible
+	github.com/onsi/ginkgo v1.14.0
+	github.com/spf13/cobra v1.2.1
+	github.com/spf13/pflag v1.0.5
+	k8s.io/api v0.22.3 //ct
+	k8s.io/apimachinery v0.22.3
+	k8s.io/apiserver v0.22.3
+	k8s.io/client-go v12.0.0+incompatible
+	k8s.io/code-generator v0.22.3
+	k8s.io/component-base v0.22.3
+	k8s.io/controller-manager v0.22.3
+	k8s.io/gengo v0.0.0-20201214224949-b6c5ce23f027
+	k8s.io/klog v1.0.0
+	k8s.io/kube-openapi v0.0.0-20211109043538-20434351676c
+	k8s.io/kubernetes v1.22.4
+	k8s.io/utils v0.0.0-20210819203725-bdf08cb9a70a
+	tkestack.io/tke v0.0.0-00010101000000-000000000000
+)
